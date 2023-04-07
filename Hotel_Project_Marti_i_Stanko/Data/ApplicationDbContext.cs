adff36ec3d90272737dcaf00d3621e68b62826a0
Мартина Пеева <martitopp14@gmail.com>
2023-04-07 20:01:08 +0300

    adding things in start up

--- Hotel_Project_Marti_i_Stanko/Data/ApplicationDbContext.cs
+++ Hotel_Project_Marti_i_Stanko/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Hotel_Project_Marti_i_Stanko.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,6 @@ namespace Hotel_Project_Marti_i_Stanko.Data
             : base(options)
         {
         }
-    }
+        public DbSet<Room> Rooms { get; set; }
+}
 }
--- Hotel_Project_Marti_i_Stanko/Data/Migrations/20230407161607_Initial.Designer.cs
+++ Hotel_Project_Marti_i_Stanko/Data/Migrations/20230407161607_Initial.Designer.cs
@@ -4,14 +4,16 @@ using Hotel_Project_Marti_i_Stanko.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Hotel_Project_Marti_i_Stanko.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230407161607_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
