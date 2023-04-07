fcae544380e1d0c6ecc88e2481cc4be090f47af6
Мартина Пеева <martitopp14@gmail.com>
2023-04-07 19:01:00 +0300

    added entity Room and enums for room type

--- Hotel_Project_Marti_i_Stanko/Models/Room.cs
+++ Hotel_Project_Marti_i_Stanko/Models/Room.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Hotel_Project_Marti_i_Stanko.Models.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +11,17 @@ namespace Hotel_Project_Marti_i_Stanko.Models
     {
         public int ID { get; set; }
         public int capacity { get; set; }
+
+        [Column("RoomType")]
+        public string roomType { get; set; }
+
+        public void SetType(RoomType roomType1)
+        {
+            roomType = roomType1.ToString();
+        }
+        public bool isOccupied { get; set; }
+        public double priceForAdult { get; set; }
+        public double priceForChild { get; set; }
+        public int number { get; set; }
     }
 }
