0747f403d38848cc45478608414013c0a99589b9
Мартина Пеева <martitopp14@gmail.com>
2023-04-07 18:04:53 +0300

    Add project files.

---
+++ Hotel_Project_Marti_i_Stanko/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel_Project_Marti_i_Stanko.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Hotel_Project_Marti_i_Stanko/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel_Project_Marti_i_Stanko.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public int capacity { get; set; }
+    }
+}
